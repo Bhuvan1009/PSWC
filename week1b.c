5b814ef3602c20117a1d2f13e701d7940a6b8883
Bhuvan1009 <158060711+Bhuvan1009@users.noreply.github.com>
2024-12-29 17:14:30 +0530

    Create week1b.c

---
+++ week1b.c
@@ -0,0 +1,10 @@
+// Area of circle
+#include<stdio.h>
+void main()
+{
+    float p,r,area;
+    printf("Enter p,r values\n");
+    scanf("%f%f",&p,&r);
+    area=p*r*r;
+    printf("Area of circle=%f",area);
+}
