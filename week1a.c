42fe938b0d9f11e354d157383020a112de57a1aa
Bhuvan1009 <158060711+Bhuvan1009@users.noreply.github.com>
2024-12-29 17:13:37 +0530

    Create week1a.c

---
+++ week1a.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int p,t,r,si;
+printf("enter principle,time,rate of interst");
+scanf("%d%d%d",&p,&t,&r);
+si=(p*t*r)/100;
+printf("%f simple interst", is %f,si);
+}
